aedab1676d9badb7c477774de4838c5a1bcb5abb
TheKillerSushi <116966538+TheKillerSushi@users.noreply.github.com>
2024-11-14 20:13:19 -0500

    Update story.py

--- story.py
+++ story.py
@@ -1,5 +1,5 @@
 # Imported Libraries
-import filters
+
 
 
 # Global variables
@@ -31,17 +31,16 @@ def start_story(): # Starts the story and runs the entirety of it
 
         # Question, [choice1, filter1], [choice2, fitler2]
         ALL_STORY_BEATS = [
-            ["When you arrive at the beach, you see two areas you could photograph. Which one do you want to take a photo of?", ["Photograph the crystal blue ocean","BLUE"], ["2 Photograph the beach, the golden sand, pristine and undisturbed","YELLOW"]],
+            ["When you arrive at the beach, you see two areas you could photograph. Which one do you want to take a photo of?", ["1 Photograph the crystal blue ocean","BLUE"], ["2 Photograph the beach, the golden sand, pristine and undisturbed","YELLOW"]],
             []
         ]
 
         choices_made = 0
         for situation in ALL_STORY_BEATS:
-            print("\n",situation[0])
             # What would user like to do?
             user_choice = ""
             while user_choice not in ["1","2","stop"]:
-                user_choice = input(f"1 {situation[1][0]}\n{situation[2][0]}\n").lower()
+                user_choice = input("What would you like to do?\n1 Return to start screen\n2 View FAQ\n\n").lower()
 
             # User typed stop and this function returns 3 to mark the programs end. 1 chooses the first option and 2 chooses the second.
             if user_choice == "stop":
@@ -49,7 +48,7 @@ def start_story(): # Starts the story and runs the entirety of it
             
             print("You chose to ", situation[int(user_choice)][1],"\n")
             choices_made += 1
-            all_user_choices[choices_made] = situation[int(user_choice)][0]
+            all_user_choices[choices_made] = situation[int(user_choice)][1]
             
             # FILTER LOGIC HERE
             
@@ -87,22 +86,21 @@ def create_start_state(): # This function prints the start screen of the story a
     
     print("FILTER\n\n")
 
-    print('Type the number corresponding to the choice you want to make or type "stop" at anytime to exit the program.\n\n')
+    print('Type "stop" at anytime to exit the program.\n\n')
 
     # Taking user input to move to another 
     user_choice = ""
     while user_choice != "stop":
-        while user_choice not in ["1", "2", "stop"]:
+        while user_choice not in ["1", "2", "3", "stop"]:
 
             # Obtains user input and transfers it to lowercase to account for capitalization cases
-            user_choice = input("Please choose an option:\n1 Start the story\n2 Help\n\n").lower()
+            user_choice = input("Please choose an option:\n1 Start the story\n2 Help\n3 Settings\n\n").lower()
     
         if user_choice == "1":
 
             # start the story
             # if statement is trying to figure out if the user typed stop
-            story_state = start_story()
-            if story_state == 3:
+            if start_story() == 3:
                 return 0
 
 
@@ -114,6 +112,13 @@ def create_start_state(): # This function prints the start screen of the story a
                 return 0
 
 
+        elif user_choice == "3":
+
+            # start the settings page
+            # if statement is trying to figure out if the user typed stop
+            if start_settings() == 3:
+                return 0
+        user_choice = ""
     # user has typed stop, end the program.
     return 0 
 create_start_state()
