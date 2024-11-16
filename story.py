43ebebf5c296d033f68b21af5e70a8d026f7a991
TheKillerSushi <116966538+TheKillerSushi@users.noreply.github.com>
2024-11-15 21:35:56 -0500

    Update story.py

--- story.py
+++ story.py
@@ -5,42 +5,34 @@ import math
 
 # Global variables
 
-image = Image.open("Beach.jpg")
-size = image.size
-image = filters.black_white_filter(image)
-image.save("test.jpg")
-image.close()
-# Make sure to include the result of the choice a list [prompt (short), choice, age]
+
 all_user_choices = {} # This variable stores every user choice (that is significant) so that these choices can be made into a report
 
 # Story procedures functions
 
-def generate_user_choices(all_user_choices): # This function generates a report of the users main choices and actions throughout the story
+def generate_user_choices(all_user_choices, choices_made): # This function generates a report of the users main choices and actions throughout the story
 
-    if len(all_user_choices) == 0:
+    if choices_made == 0:
         print("No choices Made!")
         return 1
     else:
+        print("REPORT:\n")
         for choice in all_user_choices:
-            print(f"You decided to {all_user_choices[choice[1]]} when presented the option to {all_user_choices[choice[0]]} when you were {all_user_choices[choice[2]]}")
+            print(f"You decided to {all_user_choices[choice][0]} causing your image to be filtered with the {all_user_choices[choice][1]} filter\n")
+    input("click enter to continue")
 
 def start_story(): # Starts the story and runs the entirety of it
         
+        # IMAGE to filter
+        image = Image.open("Beach.jpg")
+        size = image.size
 
-    
-        # all the fiter functions
-        filters = {}
-
-        # What filters are being used 
-        filters_in_use = []
-        
-        # Each image (with filters included)
-        images = {}
 
         # Question, [choice1, filter1], [choice2, fitler2]
         ALL_STORY_BEATS = [
-            ["You are a newly 8 year old boy. It is your birthday, and as a present, you are given a camera by your parents. You are going to the beach to celebrate and you decide to bring the camera into what will become your lifelong passion. These are the some of the most important moments in your life. When you arrive at the beach, you see two areas you could photograph. Which one do you want to take a photo of?", ["Photograph the crystal blue ocean","blue"], ["Photograph the beach, the golden sand, pristine and undisturbed","yellow"]],
-            ["After that day at the beach, you began photographing more things, particularly birds. As you grew, you reaelized that you hadn't really taken any photos that represented you. Now at the age of 12, you decided you wanted to start taking pictures that were more personal to you. Now what idea would be more personally significant to you?", ["Photograph your house, a place you can always feel safe, and a place you know you can return to no matter what", "sepia"], ["Photograph your bedroom, your HQ, where you can connect with your freinds from online, and where you can be alone", "black_white_"]]
+            ["You are a newly 8 year old boy. It is your birthday, and as a present, you are given a camera by your parents. You are going to the beach to celebrate and you decide to bring the camera into what will become your lifelong passion. These are the some of the most important moments in your life. When you arrive at the beach, you see two areas you could photograph. Which one do you want to take a photo of?", ["Photograph the crystal blue ocean", "NONE"], ["Photograph the beach, the golden sand, pristine and undisturbed","NONE"]],
+            ["After that day at the beach, you began photographing more things, particularly birds. As you grew, you reaelized that you hadn't really taken any photos that represented you. Now at the age of 12, you decided you wanted to start taking pictures that were more personal to you. Now what idea would be more personally significant to you?", ["Photograph your house, a place you can always feel safe, and a place you know you can return to no matter what", "sepia"], ["Photograph your bedroom, your HQ, where you can connect with your freinds from online, and where you can be alone", "gray"]],
+            ["As a 16 year old, you began to branch out and hang out wiht more people, but after a fall you woke up and realized that you couldn't remember what had happened earlier in your life. Now, with your memorys fading forever, you need to choose how you want to remember your life thus far. What will you do?", ["Focus on your past and maybe miss some details", "dither"], ["Let your memory's fade, and keep whatever parts of the memory's you can hold onto", "blur"]]
         ]
 
         choices_made = 0
@@ -55,15 +47,43 @@ def start_story(): # Starts the story and runs the entirety of it
             if user_choice == "stop":
                 return 3
             
-            print("You chose to ", situation[int(user_choice)][0],"\n")
+            # Filtering based on user choice
+            user_choice_int = int(user_choice)
+
+            #if situation[user_choice_int][1] == "blue":
+                #image = filters.blue_filter(image)
+            #elif situation[user_choice_int][1] == "yellow":
+                #image = filters.yellow_filter(image)
+            if situation[user_choice_int][1] == "sepia":
+                image = filters.sepia_filter(image)
+            elif situation[user_choice_int][1] == "gray":
+                image = filters.gray_filter(image)
+            elif situation[user_choice_int][1] == "blur":
+                image = filters.blur(image)
+            elif situation[user_choice_int][1] == "dither":
+                image = filters.dither(image)
+    
+
+            print("You chose to ", situation[user_choice_int][0],"\n")
             choices_made += 1
-            all_user_choices[choices_made] = situation[int(user_choice)][0]
+            all_user_choices[choices_made] = situation[user_choice_int]
+        
+            image.save("test.jpg")
+        
+        print("After all you had been through, and with what memory's you retained, you rediscovered your first photo ever and saw it thorugh a new lens. A new filter. How did your experiences shape what you see?")
+        
+        # Show the image to the user and closes it in the actual code
+        image.show()
+        image.close()
+        generate_user_choices(all_user_choices,choices_made)
+        
+
             
         
 
             
 def start_help():
-    print("--------------------")
+
     print("Help\n")
 
     # What would user like to do?
@@ -73,9 +93,9 @@ def start_help():
 
     # All Frequently asked questions [question, answer format]
     FAQ = [["How long is this story?", "This story is short and can be completed in a few minutes."],
-           ["Do your choices actually have an impact on the outcome of the story?", "Every choice has an impact on the final remarks of the story and the final images you get."],
-           ["Are there branching paths in this game?", "This story does not have branching paths, but it does have many possible combinations of pictures and filters for your images."],
-           ["Why do the images get filtered?", "The pictures get filtered as an artistic design choice and to elevate the symbolism and ideas of the story."]]
+           ["Do your choices actually have an impact on the outcome of the story?", "The story mostly surrounds remembering your past with the biases of the future, and the final image is a reflection of that, and thus is the ending of the story in of itself meaning that your choices DO have an imapact on the outcome of the story."],
+           ["Are there branching paths in this game?", "This story does not have branching paths, but it does have many possible combinations of choices you make which can lead to a brand new final image every time."],
+           ["Why does the image get filtered?", "The picture gets filtered as an artistic design choice and to elevate the symbolism and ideas of the story which can make the end of the story more substantial by not outright concluding the story but allowing the user to SEE the outcome of their choices."]]
     # User typed stop and this function returns 3 to mark that for other functions to use.
     if user_choice == "stop":
         return 3
@@ -87,15 +107,17 @@ def start_help():
             print("Question: ", i[0], "\nAnswer: ", i[1], "\n\n")
 
 def create_start_state(): # This function prints the start screen of the story and presents the user with options on what they want to do
-    print("--------------------")
-    
-    print("FILTER\n\n")
-
-    print('Type the number corresponding to the choice you want to make or type "stop" at anytime to exit the program.\n\n')
-
-    # Taking user input to move to another 
     user_choice = ""
     while user_choice != "stop":
+    
+        print("FILTER\n\n")
+
+        print('Type the number corresponding to the choice you want to make or type "stop" at anytime to exit the program.\n\n')
+
+        # Taking user input to move to another 
+        
+
+        user_choice = ""
         while user_choice not in ["1", "2", "stop"]:
 
             # Obtains user input and transfers it to lowercase to account for capitalization cases
